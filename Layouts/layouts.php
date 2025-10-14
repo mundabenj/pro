20dfc96ae9a275eb1109b1b6c1448e2a886381c6
ALPHAWOLF\munda <benjamin.mundama@strathmore.edu>
2025-10-14 18:22:17 +0300

    implemented File export v2

--- Layouts/layouts.php
+++ Layouts/layouts.php
@@ -126,9 +126,6 @@ public function content() {
 
       <!-- for the tables -->
        
-<!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script> -->
-
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.3.4/js/dataTables.bootstrap5.js"></script>
@@ -164,13 +161,12 @@ public function content() {
                   { "data": 3 }
                ],
                responsive: true,
-            layout: {
-               topStart: {
-                  buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+               layout: {
+                  topStart: {
+                     buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+                  }
                }
-            }
-         });
-
+            });
          <?php
       }
    }
